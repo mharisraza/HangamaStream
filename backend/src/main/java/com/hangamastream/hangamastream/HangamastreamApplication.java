172d069058a256e8a1617a38881db8816c59c9ef
Muhammad Haris <wannaheadshot5@gmail.com>
2023-04-11 00:50:40 +0500

    Implemented initialization

---
+++ backend/src/main/java/com/hangamastream/hangamastream/HangamastreamApplication.java
@@ -0,0 +1,13 @@
+package com.hangamastream.hangamastream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HangamastreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HangamastreamApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/hangamastream/hangamastream/HangamastreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hangamastream.hangamastream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HangamastreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
