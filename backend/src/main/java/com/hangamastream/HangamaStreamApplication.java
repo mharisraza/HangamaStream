c3c17ce784d62110ccd75e84edc7ef989ac0286f
Muhammad Haris <wannaheadshot5@gmail.com>
2023-04-11 01:29:40 +0500

    update project structure

--- backend/src/main/java/com/hangamastream/HangamaStreamApplication.java
+++ backend/src/main/java/com/hangamastream/HangamaStreamApplication.java
@@ -1,13 +1,13 @@
-package com.hangamastream.hangamastream;
+package com.hangamastream;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HangamastreamApplication {
+public class HangamaStreamApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HangamastreamApplication.class, args);
+		SpringApplication.run(HangamaStreamApplication.class, args);
 	}
 
 }
