69e178fcd44a775f7cb051bdec394401ca4fdbbd
Muhammad Haris <wannaheadshot5@gmail.com>
2023-04-13 02:35:04 +0500

    Configured async

---
+++ backend/src/main/java/com/hangamastream/config/AsyncConfiguration.java
@@ -0,0 +1,17 @@
+package com.hangamastream.config;
+
+import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@Configuration
+@EnableAsync  
+public class AsyncConfiguration implements AsyncConfigurer {
+
+    @Override
+    public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
+        return new AsyncExceptionHandler();
+    }
+    
+}
