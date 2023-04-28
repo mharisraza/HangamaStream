be90e3b5c05341d0d09c884eae38da11dcf1cfc1
Muhammad Haris <wannaheadshot5@gmail.com>
2023-04-28 14:36:43 +0500

    initialize frontend

---
+++ frontend/src/pages/_app.tsx
@@ -0,0 +1,17 @@
+import type { AppProps } from "next/app";
+import Head from "next/head";
+import "../styles/globals.css";
+import axios from 'axios';
+
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+    <Head>
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossOrigin="anonymous" />
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
+    </Head>
+      <Component {...pageProps} />
+    </>
+  );
+}
---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import '@/styles/Home.module.css'
+import Navbar from '@/components/Navbar'
+import Base from '@/components/Base'
+
+export default function Home() {
+  return (
+    <>  
+    <Base title='HangamaStream - Home'>
+    </Base>
+    </>
+  )
+}
